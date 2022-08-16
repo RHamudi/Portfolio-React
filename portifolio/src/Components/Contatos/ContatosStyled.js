953822cdace599cc475598ff9c1ad43e2959304b
RHamudi <ramonramos.silva16@gmail.com>
2022-08-16 16:47:48 -0300

    Ajustes css background

--- portifolio/src/Components/Contatos/ContatosStyled.js
+++ portifolio/src/Components/Contatos/ContatosStyled.js
@@ -1,14 +1,12 @@
 import styled from "styled-components";
 
 export const H1 = styled.h1`
+    margin-bottom: 4rem;
     text-align: center;
     color: white;
 `;
 
 export const ContainerContatos = styled.section`
-    position: absolute;
-    width: 100%;
-    height: calc(100% - 110px);
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
